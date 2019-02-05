250a899ece16c4d3cc45a86f5b9b13cb226be889
James Burry <jamescburry@gmail.com>
2019-02-05 11:26:27 +0000

    [JB] Score comparator functional added

--- src/main/scala/App/PredictorApplication.scala
+++ src/main/scala/App/PredictorApplication.scala
@@ -3,10 +3,10 @@ package App
 import scala.math._
 import scala.io.StdIn
 
-object GreeterApplication extends App {
+object PredictorApplication extends App {
   def greet(name: String): Unit = println()
 
-  var name: String = StdIn.readLine("What's your name? ")
+  var name: String = StdIn.readLine("Enter Username: ")
   greet(name)
   acc(name)
 
@@ -14,19 +14,19 @@ object GreeterApplication extends App {
   def acc(name: String): Unit =
     if (name != "James") {
       println(s"Denied")
-      var name: String = StdIn.readLine("What's your name? ")
+      var name: String = StdIn.readLine("Enter Username: ")
       acc(name)
     }
     else
       println(s"Accepted")
       def team1(t1: String): Unit = println()
-      var t1: String = StdIn.readLine("Enter team 1 ")
+      var t1: String = StdIn.readLine("Enter team 1: ")
 
 
 
 
       def team2(t2: String): Unit = println()
-      var t2: String = StdIn.readLine("Enter team 2 ")
+      var t2: String = StdIn.readLine("Enter team 2: ")
 
 
 
@@ -43,8 +43,16 @@ object GreeterApplication extends App {
       val t2s = {
         ceil(Math.random * 5 + 1)
   }
-      println(t1 + " " + t1s + "----" + t2s + " " + t2)
-
+      println(t1 + " " + t1s + " ---- " + t2s + " " + t2)
+      if(t1s > t2s){
+        println(t1 + " wins!")
+      }
+      else if(t1s < t2s) {
+        println(t2 + " wins!")
+      }
+      else{
+        println("Draw!")
+      }
 
 
 
