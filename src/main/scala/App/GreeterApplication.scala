e8032a5011bde1f22c7cfa0c877e5c563377e6d8
James Burry <jamescburry@gmail.com>
2019-02-05 10:52:55 +0000

    Added score functional

--- src/main/scala/App/GreeterApplication.scala
+++ src/main/scala/App/GreeterApplication.scala
@@ -2,24 +2,27 @@ package App
 
 import scala.io.StdIn
 
+
   object GreeterApplication extends App {
-    def greet(name: String): Unit = println(s"Hello $name")
+    def greet(name: String): Unit = println()
 
     var name: String = StdIn.readLine("What's your name? ")
     greet(name)
     acc(name)
 
 
-
     def acc(name: String): Unit =
-      if (name != "James"){
+      if (name != "James") {
         println(s"Denied")
         var name: String = StdIn.readLine("What's your name? ")
-        acc(name)}
-      else{
-        println(s"Accepted")}}
+        acc(name)
+      }
+      else
+        println(s"Accepted")}
 
-    def num(rand : Int)
+
+
+
 
 
 
---
+++ src/main/scala/App/team.scala
@@ -0,0 +1,37 @@
+package App
+
+import scala.math._
+import scala.io.StdIn
+import scala.util.Random
+
+object team extends App {
+
+
+
+  def team1(t1: String): Unit = println()
+  var t1: String = StdIn.readLine("Enter team 1 ")
+
+
+
+
+  def team2(t2: String): Unit = println()
+  var t2: String = StdIn.readLine("Enter team 2 ")
+
+
+
+
+  def score1(t1s: String): Unit = println()
+  val t1s = {
+    floor(Math.random * 5 + 1)
+  }
+
+
+
+
+  def score2(t2s: String): Unit = println()
+  val t2s = {
+    floor(Math.random * 5 + 1)
+  }
+
+}
+
