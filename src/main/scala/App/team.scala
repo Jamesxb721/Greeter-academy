3a72cda5b0c29a9f62f256d2b961aee215a0b322
James Burry <jamescburry@gmail.com>
2019-02-05 11:04:10 +0000

    [JB] Compiled username and score functionals

--- src/main/scala/App/team.scala
+++ src/main/scala/App/team.scala
@@ -2,36 +2,53 @@ package App
 
 import scala.math._
 import scala.io.StdIn
-import scala.util.Random
 
-object team extends App {
+object GreeterApplication extends App {
+  def greet(name: String): Unit = println()
 
-
-
-  def team1(t1: String): Unit = println()
-  var t1: String = StdIn.readLine("Enter team 1 ")
-
+  var name: String = StdIn.readLine("What's your name? ")
+  greet(name)
+  acc(name)
 
 
+  def acc(name: String): Unit =
+    if (name != "James") {
+      println(s"Denied")
+      var name: String = StdIn.readLine("What's your name? ")
+      acc(name)
+    }
+    else
+      println(s"Accepted")
+      def team1(t1: String): Unit = println()
+      var t1: String = StdIn.readLine("Enter team 1 ")
 
-  def team2(t2: String): Unit = println()
-  var t2: String = StdIn.readLine("Enter team 2 ")
+
+
+
+      def team2(t2: String): Unit = println()
+      var t2: String = StdIn.readLine("Enter team 2 ")
 
 
 
 
-  def score1(t1s: String): Unit = println()
-  val t1s = {
-    floor(Math.random * 5 + 1)
+      def score1(t1s: Int): Unit = println()
+      val t1s = {
+        ceil(Math.random * 5 + 1)
   }
 
 
 
 
-  def score2(t2s: String): Unit = println()
-  val t2s = {
-    floor(Math.random * 5 + 1)
+      def score2(t2s: Int): Unit = println()
+      val t2s = {
+        ceil(Math.random * 5 + 1)
   }
+      println(t1 + " " + t1s + "----" + t2s + " " + t2)
+
+
+
+
+
 
 }
 
