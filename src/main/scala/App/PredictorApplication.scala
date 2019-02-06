fca65ec6d2459c659abd1c3c8a142edcdc904dc0
James Burry <jamescburry@gmail.com>
2019-02-06 09:41:22 +0000

    [JB] Sport Selection functional added

--- src/main/scala/App/PredictorApplication.scala
+++ src/main/scala/App/PredictorApplication.scala
@@ -1,7 +1,7 @@
 package App
 
-import scala.math._
 import scala.io.StdIn
+import scala.math.ceil
 
 object PredictorApplication extends App {
   def greet(name: String): Unit = println()
@@ -14,49 +14,69 @@ object PredictorApplication extends App {
   def acc(name: String): Unit =
     if (name != "James") {
       println(s"Denied")
-      var name: String = StdIn.readLine("Enter Username: ")
+      val name: String = StdIn.readLine("Enter Username: ")
       acc(name)
     }
     else
       println(s"Accepted")
-      def team1(t1: String): Unit = println()
-      var t1: String = StdIn.readLine("Enter team 1: ")
+  def team1(t1: String): Unit = println()
+  var t1: String = StdIn.readLine("Enter team 1: ")
+  def team2(t2: String): Unit = println()
+  var t2: String = StdIn.readLine("Enter team 2: ")
+  def sport(ws: String): Unit =println()
+  var st: String = StdIn.readLine("Which Sport? (FOOTBALL - 1, BASKETBALL - 2 or RUGBY - 3)")
+  if(st == "1"){
+    def score1(t1s: Int): Unit = println()
+    val t1s = ceil(Math.random * 3 + 1)
+    def score2(t2s: Int): Unit = println()
+    val t2s = ceil(Math.random * 3 + 1)
+    println(t1 + " - " + t1s + " --- " + t2s + " - " + t2)
+    if (t1s > t2s) {
+      println(t1 + " wins!")
+    }
+    else if (t1s < t2s) {
+      println(t2 + " wins!")
+    }
+    else {
+      println("Draw!")
+    }}
+  else if(st == "2"){
+    def score1(t1s: Int): Unit = println()
+    val t1s = ceil(Math.random * 209 + 1)
+    def score2(t2s: Int): Unit = println()
+    val t2s = ceil(Math.random * 209 + 1)
+    println(t1 + " - " + t1s + " --- " + t2s + " - " + t2)
+    if (t1s > t2s) {
+      println(t1 + " wins!")
+    }
+    else if (t1s < t2s) {
+      println(t2 + " wins!")
+    }
+    else {
+      println("Draw!")
+    }}
+  else {
+    def score1(t1s: Int): Unit = println()
+
+    val t1s = ceil(Math.random * 49 + 1)
+
+    def score2(t2s: Int): Unit = println()
+
+    val t2s = ceil(Math.random * 49 + 1)
+    println(t1 + " - " + t1s + " --- " + t2s + " - " + t2)
+    if (t1s > t2s) {
+      println(t1 + " wins!")
+    }
+    else if (t1s < t2s) {
+      println(t2 + " wins!")
+    }
+    else {
+      println("Draw!")
+    }
 
 
 
 
-      def team2(t2: String): Unit = println()
-      var t2: String = StdIn.readLine("Enter team 2: ")
-
-
-
-
-      def score1(t1s: Int): Unit = println()
-      val t1s = {
-        ceil(Math.random * 5 + 1)
   }
-
-
-
-
-      def score2(t2s: Int): Unit = println()
-      val t2s = {
-        ceil(Math.random * 5 + 1)
-  }
-      println(t1 + " " + t1s + " ---- " + t2s + " " + t2)
-      if(t1s > t2s){
-        println(t1 + " wins!")
-      }
-      else if(t1s < t2s) {
-        println(t2 + " wins!")
-      }
-      else{
-        println("Draw!")
-      }
-
-
-
-
-
 }
 
