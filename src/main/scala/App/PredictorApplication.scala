016e317d05d8c31c7d9c46ed94e0c0c51ac3ddd4
James Burry <jamescburry@gmail.com>
2019-02-06 11:16:06 +0000

    [JB/LM]moved prediction logic, refactored to case switch in app

--- src/main/scala/App/PredictorApplication.scala
+++ src/main/scala/App/PredictorApplication.scala
@@ -1,15 +1,17 @@
 package App
 
+
 import scala.io.StdIn
-import scala.math.ceil
+
 
 object PredictorApplication extends App {
+
+
   def greet(name: String): Unit = println()
 
   var name: String = StdIn.readLine("Enter Username: ")
   greet(name)
   acc(name)
-
 
   def acc(name: String): Unit =
     if (name != "James") {
@@ -19,64 +21,26 @@ object PredictorApplication extends App {
     }
     else
       println(s"Accepted")
+
+
   def team1(t1: String): Unit = println()
+
   var t1: String = StdIn.readLine("Enter team 1: ")
+
   def team2(t2: String): Unit = println()
+
   var t2: String = StdIn.readLine("Enter team 2: ")
-  def sport(ws: String): Unit =println()
-  var st: String = StdIn.readLine("Which Sport? (FOOTBALL - 1, BASKETBALL - 2 or RUGBY - 3)")
-  if(st == "1"){
-    def score1(t1s: Int): Unit = println()
-    val t1s = ceil(Math.random * 3 + 1)
-    def score2(t2s: Int): Unit = println()
-    val t2s = ceil(Math.random * 3 + 1)
-    println(t1 + " - " + t1s + " --- " + t2s + " - " + t2)
-    if (t1s > t2s) {
-      println(t1 + " wins!")
-    }
-    else if (t1s < t2s) {
-      println(t2 + " wins!")
-    }
-    else {
-      println("Draw!")
-    }}
-  else if(st == "2"){
-    def score1(t1s: Int): Unit = println()
-    val t1s = ceil(Math.random * 209 + 1)
-    def score2(t2s: Int): Unit = println()
-    val t2s = ceil(Math.random * 209 + 1)
-    println(t1 + " - " + t1s + " --- " + t2s + " - " + t2)
-    if (t1s > t2s) {
-      println(t1 + " wins!")
-    }
-    else if (t1s < t2s) {
-      println(t2 + " wins!")
-    }
-    else {
-      println("Draw!")
-    }}
-  else {
-    def score1(t1s: Int): Unit = println()
 
-    val t1s = ceil(Math.random * 49 + 1)
 
-    def score2(t2s: Int): Unit = println()
+  def sport(ws: String): Unit = println()
 
-    val t2s = ceil(Math.random * 49 + 1)
-    println(t1 + " - " + t1s + " --- " + t2s + " - " + t2)
-    if (t1s > t2s) {
-      println(t1 + " wins!")
-    }
-    else if (t1s < t2s) {
-      println(t2 + " wins!")
-    }
-    else {
-      println("Draw!")
+  val st: String = StdIn.readLine("Which Sport? (FOOTBALL - 1, BASKETBALL - 2 or RUGBY - 3)")
+  st match {
+      case "1" => predictLogic.numberOne()
+      case "2" => predictLogic.numberTwo()
+      case _   => predictLogic.anyOtherNumber()
     }
 
 
-
-
-  }
 }
 
