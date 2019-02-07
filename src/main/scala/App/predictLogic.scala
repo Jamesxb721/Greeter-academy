360203b26ee05e1352a19d3f1da2050387ea7add
James Burry <jamescburry@gmail.com>
2019-02-07 14:03:14 +0000

    [JB] Updated math logic

--- src/main/scala/App/predictLogic.scala
+++ src/main/scala/App/predictLogic.scala
@@ -7,22 +7,28 @@ import App.PredictorApplication._
 object predictLogic {
 
   var calculation = 0
+  var start = 0
+  var end = 0
+
 
   def sportType(st: String): Unit = {
 
 
     st match {
-      case "1" => calculation = 3
-      case "2" => calculation = 189
-      case "3" => calculation = 49
+      case "1" => start = 0
+                  end = 5
+      case "2" => start = 50
+                  end = 160
+      case "3" => start = 5
+                  end = 50
 
     }
     scoreGenerator()
   }
 
   def scoreGenerator(): Unit = {
-    val t1s = ceil(Math.random * calculation)
-    val t2s = ceil(Math.random * calculation)
+    val t1s = start + scala.util.Random.nextInt((end-start)+1)
+    val t2s = start + scala.util.Random.nextInt((end-start)+1)
     println(t1 + " - " + t1s + " --- " + t2s + " - " + t2)
 
 
@@ -35,6 +41,7 @@ object predictLogic {
     else {
       println("Draw!")
     }
+
 
   }
 
